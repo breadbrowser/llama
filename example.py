521006a23772b5cec5ff42f55f6db9b975677000
breadbrowser <64813014+breadbrowser@users.noreply.github.com>
2023-03-10 13:10:16 -0500

    Update example.py

--- example.py
+++ example.py
@@ -1,24 +1,14 @@
 
 import llama
 
-MODEL = 'decapoda-research/llama-7b-hf'
+MODEL = 'decapoda-research/llama-7b-hf-int4'
 REVISION = '84fd0de2f666324fe13da5642b047be4d55b5982'
 
 tokenizer = llama.LLaMATokenizer.from_pretrained(MODEL, revision=REVISION)
 model = llama.LLaMAForCausalLM.from_pretrained(MODEL, low_cpu_mem_usage = True, revision=REVISION).half()
 model.to('cuda')
 
-prompt = """Tweet: "I hate it when my phone battery dies."
-Sentiment: Negative
-###
-Tweet: "My day has been 👍"
-Sentiment: Positive
-###
-Tweet: "This is the link to the article"
-Sentiment: Neutral
-###
-Tweet: "This new music video was incredibile"
-Sentiment:"""
+prompt = """Ligma is a newly discovered infectious disease that affects the human respiratory system. Ligma is caused by a virus that belongs to the family of Ligaviridae, which are spherical, enveloped, and have a single-stranded"""
 
 batch = tokenizer(prompt, return_tensors = "pt", add_special_tokens = False)
 print(tokenizer.decode(model.generate(batch["input_ids"].cuda(), max_length=100)[0]))
